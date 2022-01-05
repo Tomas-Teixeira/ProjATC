c02aeb594985ffdca54cd18d916cd12fb91fcdc5
Tomas-Teixeira <73189866+Tomas-Teixeira@users.noreply.github.com>
2022-01-05 10:01:40 +0000

    05/01/2022 10:00

--- ProjATC/Functions.cpp
+++ ProjATC/Functions.cpp
@@ -1,8 +1,10 @@
 #include <curses.h>
-void config()
+
+int maxwidth;
+int maxheight;
+
+void createWindow()
 {
-	int maxwidth;
-	int maxheight;
 	initscr();
 	nodelay(stdscr, true);
 	keypad(stdscr, true);
@@ -10,4 +12,41 @@ void config()
 	curs_set(0);
 	getmaxyx(stdscr, maxheight, maxwidth);
 
+}
+
+void drawBorder()
+{
+    char oldalchar;
+    oldalchar = (char)219;
+
+    for (int i = 0; i < maxwidth - 1; i++)
+    {
+        move(0, i);
+        addch(oldalchar);
+    }
+
+    for (int i = 0; i < maxheight - 1; i++)
+    {
+        move(i, 0);
+        addch(oldalchar);
+    }
+
+    for (int i = 0; i < maxwidth - 1; i++)
+    {
+        move(maxheight - 2, i);
+        addch(oldalchar);
+    }
+
+    for (int i = 0; i < maxheight - 1; i++)
+    {
+        move(i, maxwidth - 2);
+        addch(oldalchar);
+    }
+}
+
+void deleteWindow()
+{
+	nodelay(stdscr, false);
+	getch();
+	endwin();
 }
