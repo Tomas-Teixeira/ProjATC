0d1d0ce6622b9a43e330baf13ec6e19ba1477215
Malibruno <62077911+Malibruno@users.noreply.github.com>
2022-01-05 22:57:48 +0000

    Update SpaceShip.cpp

--- ProjATC/SpaceShip.cpp
+++ ProjATC/SpaceShip.cpp
@@ -1,31 +1,70 @@
 #include "SpaceShip.h"
 
-void Spaceship::moveSpaceship()
-{
-    int tmp = getch();
-    switch (tmp)
-    {
-        //Move Left
-    case KEY_LEFT:
-        if (pos_x < /*Left_Limit*/)
-        {
-            pos_x++;
-        }
-        break;
+void move_nave(int ch) {
 
-        //Move Right
-    case KEY_RIGHT:
-        if (pos_x < /*Right_Limit*/)
-        {
-            pos_x--;
-        }
-        break;
-        //Shoot
-    case KEY_ENTER:
-        //Call function shoot
+    int bar2x = 60, bar2y = 27, bar2directionx = 1;
+    switch (ch) {
+    case KEY_LEFT:  // user pressed up arrow key
+        bar2directionx = -1;
+        bar2x += bar2directionx;
 
         break;
+    case KEY_RIGHT:  // user pressed up arrow key
+        bar2directionx = 1;
+        bar2x += bar2directionx;
+
+        break;
+    case ERR:
+        bar2directionx = 0;
+        bar2x += bar2directionx;
+        break;
+
     }
 
 
+
+
+    if (bar2x + 3 >= w || bar2x <= 0) {
+        bar2directionx *= -1;
+        bar2x += bar2directionx;
+    }
+    else {
+        bar2x += bar2directionx;
+    }
+    mvprintw(bar2y, bar2x + 2, "^");
+    mvprintw(bar2y + 1, bar2x + 1, "0");
+    mvprintw(bar2y + 1, bar2x + 2, "0");
+    mvprintw(bar2y + 1, bar2x + 3, "0");
+    mvprintw(bar2y + 2, bar2x, "0");
+    mvprintw(bar2y + 2, bar2x + 1, "0");
+    mvprintw(bar2y + 2, bar2x + 2, "0");
+    mvprintw(bar2y + 2, bar2x + 3, "0");
+    mvprintw(bar2y + 2, bar2x + 4, "0");
+
 }
+
+
+void manda_tiros_nave(int bar2x, int ch) {
+    int tiroy = 3, ac, tirox;
+    if (ac % 10 == 0) {
+        switch (ch) {
+        case KEY_ENTER:
+            ac = 0;
+            tirox = bar2x + 1;
+            break;
+        case ERR:
+            break;
+        }
+    }
+    ac++;
+}
+
+void atualiza_tiros_nave() {
+    //Atualiza as coords dos tiros percorrendo o vetor de tiros
+}
+
+
+void damage_nave() {
+    //Se y e x do tiro do alien for igual ao da nave:
+    healthbar--;
+    //else return;
