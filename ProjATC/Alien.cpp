ec0ae7c62a6f79facddd8683b2881c8c1a6fa9e4
Malibruno <62077911+Malibruno@users.noreply.github.com>
2022-01-05 22:54:42 +0000

    Update Alien.cpp

--- ProjATC/Alien.cpp
+++ ProjATC/Alien.cpp
@@ -1,7 +1,33 @@
 #include "Alien.h"
 
-void Alien::moveAlien()
-{
+void manda_tiros_alien(int bar1x, int ch) {
+    int tiroy = 3, ac, tirox;
+    if (ac % 10 == 0) {
+        //Cria tiro e adiciona ao vetor
+        tirox = bar1x + 1;
+    }
+    ac++;
+}
 
+void atualiza_tiros_alien() {
+    //Atualiza as coords dos tiros percorrendo o vetor de tiros
+}
 
+void move_alien() {
+    if (bar1x + 5 >= w || bar1x - 3 < 0) {
+        bar1directionx *= -1;
+        bar1x += bar1directionx;
+    }
+    else {
+        bar1x += bar1directionx;
+    }
+    mvprintw(bar1y + 1, bar1x + 1, "V");
+    mvprintw(bar1y + 1, bar1x + 2, "V");
+    mvprintw(bar1y + 1, bar1x + 3, "V");
+}
+
+void damage_alien() {
+    //Se y e x do tiro da nave for igual ao alien:
+    healthbar--;
+    //else return;
 }
